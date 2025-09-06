79e55fd7d7976ef2e666a0bf063465976f778fcc
Mahmoud-kh1 <hhh116716@gmail.com>
2025-09-06 20:09:02 +0300

    window is done

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk 
+
+root = Tk()
+root.title('Employee Managment System')
+root.geometry('1310x515+100+100')
+root.resizable(False, False)
+root.configure(bg = '#2c3e50')
+root.mainloop()
