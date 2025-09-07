859714ed25c8dcf7999291d589111c872c899866
Mahmoud-kh1 <hhh116716@gmail.com>
2025-09-07 11:07:50 +0300

    database is done

---
+++ db.py
@@ -0,0 +1,44 @@
+import sqlite3
+class Database:
+    def __init__(self, db):
+        self.con = sqlite3.connect(db) 
+        self.cur = self.con.cursor()
+        
+        sql = """
+           CREATE TABLE IF NOT EXISTS employees(
+           id Integer Primary Key ,
+           name text,
+           age text,
+           job text, 
+           email text,
+           mobile text,
+           address text
+           )
+          """
+        
+        self.cur.execute(sql)
+        self.con.commit()
+
+    def insert(self, name, age, job, email,gender, mobile ,address):
+        self.cur.execute("insert into employess values (NULL, ? , ?, ? , ?, ?, ? , ?)", 
+                         (name, age , job, email , gender, mobile, address)
+             )   
+        self.con.commit()
+
+
+    def fetch(self):
+        self.cur.execute("SELECT * FROM employess")    
+        rows = self.cur.fetchall()
+        return rows
+    
+    def remove(self):
+        self.cur.execute("delete from employess where id=?", (id, ))
+        self.con.commit()
+
+
+    def update(self, id, name, age, job, email,gender, mobile ,address):
+        self.cur.execute("update  employess set name =?, age=? ,job =? ,email=?, gender  =?, mobile = ? ,address = ? where id = ?",
+                         (name, age , job, email , gender, mobile, address, id)
+                         
+                         )
+        self.con.commit()  
